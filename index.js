8d77414b221c50a1afde59c9baf0b19c7a717a51
Anupoju Mohana Rao <mohananupoju14@gmail.com>
2023-10-04 06:13:53 +0000

    Super-Over-League-solution

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div class="super-over-league">
+    <h1 class="heading">Super Over League</h1>
+    <div class="teams-section-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="image-logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="image-logo"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
